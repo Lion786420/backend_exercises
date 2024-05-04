8551385a35b70c2cad24dece544072dd8ebcb001
Lion786420 <thapaaniket12345@gmail.com>
2024-05-05 02:40:11 +0545

    Post request added with check for errors

--- index.js
+++ index.js
@@ -39,6 +39,28 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.post("/api/persons", (request, response) => {
+  const newPerson = request.body;
+  if (newPerson.name && newPerson.number) {
+    const nameFilter = persons.filter(
+      (person) => person.name.toLowerCase() == newPerson.name.toLowerCase()
+    );
+    if (nameFilter.length > 0) {
+      response.status(200).send({ error: "Name must be unique" });
+    } else {
+      const newContact = {
+        id: Math.ceil(Math.random() * 5000),
+        name: newPerson.name,
+        number: newPerson.number,
+      };
+      persons = persons.concat(newContact);
+      response.send(newContact);
+    }
+  } else {
+    response.status(404).send({ error: "Name or number missing" });
+  }
+});
+
 app.delete("/api/persons/:id", (request, response) => {
   const searchedId = Number(request.params.id);
   persons = persons.filter((person) => person.id !== searchedId);
