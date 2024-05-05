e9b91667028fe1a6fd759c17d1c6cf8ca037f7c2
Lion786420 <thapaaniket12345@gmail.com>
2024-05-05 09:34:21 +0545

    Added morgan to log requests and post request data

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
 app.use(express.json());
+
+morgan.token("postLog", (req, res) => {
+  return JSON.stringify(req.body);
+});
+app.use(morgan("tiny"));
 
 persons = [
   {
@@ -39,6 +46,22 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const searchedId = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== searchedId);
+  console.log(persons);
+  response.status(202).end();
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`
+  );
+});
+
+app.use(
+  morgan(":method :postLog :status :res[content-length] - :response-time ms")
+);
 app.post("/api/persons", (request, response) => {
   const newPerson = request.body;
   if (newPerson.name && newPerson.number) {
@@ -59,19 +82,6 @@ app.post("/api/persons", (request, response) => {
   } else {
     response.status(404).send({ error: "Name or number missing" });
   }
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const searchedId = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== searchedId);
-  console.log(persons);
-  response.status(202).end();
-});
-
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`
-  );
 });
 
 const PORT = 3001;
