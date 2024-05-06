c8182588f66f71fecdafe7374f9a7fc567bfb7e2
Lion786420 <thapaaniket12345@gmail.com>
2024-05-06 18:42:39 +0545

    Frontend dist created for phonebook app

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 
 morgan.token("postLog", (req, res) => {
@@ -84,6 +87,6 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Listening on port ${PORT}`);
